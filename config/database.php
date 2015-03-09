7de4ec9744c865f8aaa365e401208f619b5c6558
thinhit <thinhit@yahoo.com>
2015-03-10 00:09:30 +0700

    update

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'host'      => env('DB_HOST', '128.199.134.176'),
+			'database'  => env('DB_DATABASE', 'la-shop-core'),
+			'username'  => env('DB_USERNAME', 'lala'),
+			'password'  => env('DB_PASSWORD', 'lala'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
