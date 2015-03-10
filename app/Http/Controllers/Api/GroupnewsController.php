f9fb6530d43511d6b74a88b082bbcb13e4ff7514
Tran Cong Dao <daotran@daotran-HP-431-Notebook-PC>
2015-03-10 23:33:28 +0700

    test

---
+++ app/Http/Controllers/Api/GroupnewsController.php
@@ -0,0 +1,47 @@
+<?php namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use App\Http\Models as Models;
+class GroupnewsController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller renders your application's "dashboard" for users that
+	| are authenticated. Of course, you are free to change or remove the
+	| controller as you wish. It is just here to get your app started!
+	|
+	*/
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	/**
+	 * Show the application dashboard to the user.
+	 *
+	 * @return Response
+	 */
+	public function getIndex()
+	{
+		return 1;
+	}
+
+	public function getCreate(){
+		return Models\User::all();
+	}
+
+	public function getViewall() {
+		$data = Models\Group_news::all();
+		$arr = array('error' => true,'message'=>"test",'data'=>$data);
+		return json_encode($arr);
+	}
+
+}
---
+++ app/Http/Models/Group_news.php
@@ -0,0 +1,21 @@
+<?php namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+class Group_news extends Model {
+
+	
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'group_news';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+
+}
