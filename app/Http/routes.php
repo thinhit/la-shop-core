7eb8abce88f5ad26821897a18c325ec04df0e534
Tran Cong Dao <daotran@daotran-HP-431-Notebook-PC>
2015-03-10 23:32:49 +0700

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,12 +16,19 @@ Route::get('/', 'WelcomeController@index');
 Route::get('home', 'HomeController@index');
 
 
+/*API*/
 
+Route::group(['prefix' => 'api', 'middleware' => 'auth'], function()
+{	
+	Route::controller('v1/group_news', 'Api\GroupnewsController');
+
+});
+
+/*API*/
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function()
 {	
 	Route::controller('', 'Admin\MainController');
-    
 
 });
 
